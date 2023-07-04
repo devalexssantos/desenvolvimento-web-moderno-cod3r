1ceccfe658fb999039e71c7e22d062f9765534d4
devalexssantos <dev.alexssantos@gmail.com>
2023-07-03 22:08:54 -0300

    initial commit

---
+++ primeiros-passos/01-desafio-sentenca-e-bloco.js
@@ -0,0 +1,17 @@
+//Faça um bloco de código para imprimir os números pares de 1 a 10 e outro para imprimir os números ímpares no mesmo interalo
+
+{
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+}
+
+{
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+<h1>Repositório para resolver exercícios do curso de <strong>desenvolvimento web moderno</strong> da cod3r</h1>
+<p>Os exercícios tem subpastas de acordo com a evolução do curso</p>
