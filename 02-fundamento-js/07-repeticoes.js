258efcc5640f639032dde2bc684b2b48c9c4c9ac
devalexssantos <dev.alexssantos@gmail.com>
2023-07-28 16:55:26 -0300

    add aula de estruturas de repetição

---
+++ 02-fundamento-js/07-repeticoes.js
@@ -0,0 +1,19 @@
+//While
+let contador = 0;
+
+while(contador <= 10) {
+    console.log(contador);
+    contador++;
+}
+
+//Do While
+do {
+    contador--;
+    console.log(contador);
+} while(contador >= 0)
+
+//For
+for (let contador = 0; contador <= 10; contador++) {
+    console.log(contador);
+    
+}
