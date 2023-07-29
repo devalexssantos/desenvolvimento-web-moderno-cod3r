c2d286ac3ed9c34dfce22b230d008fea3e46e468
devalexssantos <dev.alexssantos@gmail.com>
2023-07-29 11:35:07 -0300

    add complemento de aula

--- 02-fundamento-js/07-repeticoes.js
+++ 02-fundamento-js/07-repeticoes.js
@@ -17,3 +17,22 @@ for (let contador = 0; contador <= 10; contador++) {
     console.log(contador);
     
 }
+
+//For in (para percorrer um array ou objeto com repetição)
+
+//Array
+let lista = ['Pedro', 'Tiago', 'João'];
+for(i in lista) {
+    console.log(`${i} - ${lista[i]}`);
+}
+
+//Objeto
+let pessoa = {
+    nome: 'Alex',
+    idade: 30,
+    casado: true,
+}
+
+for(atributo in pessoa) {
+    console.log(`${atributo} = ${pessoa[atributo]}`);
+}
